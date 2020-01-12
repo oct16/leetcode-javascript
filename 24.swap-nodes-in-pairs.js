4bad94cc30acdcb37f531860d9ef0f1ce4db1f8b
FengFan <mail@fengfan.me>
2020-01-12 12:03:05 +0800

    feat: add case 24

---
+++ 24.swap-nodes-in-pairs.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=24 lang=javascript
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head || !head.next) {
+        return head
+    }
+
+    const temp = head.next
+    head.next = swapPairs(temp.next)
+    temp.next = head
+    return temp
+}
+// @lc code=end
