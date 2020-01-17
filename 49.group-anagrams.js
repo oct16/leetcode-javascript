2867f19067eb09f055d142320e9cf6eaef6b3bac
FengFan <mail@fengfan.me>
2020-01-17 11:06:41 +0800

    feat: add case 49

---
+++ 49.group-anagrams.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const result = {}
+    for (let i = 0; i < strs.length; i++) {
+        const str = strs[i]
+
+        const sorted = Array.from(str)
+            .sort()
+            .join('')
+        if (!result[sorted]) {
+            result[sorted] = []
+        }
+        result[sorted].push(str)
+    }
+    return Object.values(result)
+}
+// @lc code=end
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
+console.log(groupAnagrams(['', '']))
