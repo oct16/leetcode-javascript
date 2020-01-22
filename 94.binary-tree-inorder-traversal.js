19ac4e7c8a5c7d9c4d520fbe302fda3cdf8761af
FengFan <mail@fengfan.me>
2020-01-22 17:09:54 +0800

    feat: add case 94

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    return root ? [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)] : []
+}
+// @lc code=end
