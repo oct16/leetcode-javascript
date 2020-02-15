5304e9b2eb330f812b9e9c5077526158f0d6d699
FengFan <mail@fengfan.me>
2020-02-15 15:01:17 +0800

    feat: add case 122

---
+++ 122.best-time-to-buy-and-sell-stock-ii.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=122 lang=javascript
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = 0
+    if (prices.length < 2) {
+        return maxProfit
+    }
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1]
+        }
+    }
+    return maxProfit
+}
+// @lc code=end
