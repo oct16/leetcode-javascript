6b730a3784601aab3a0f322d39d37ae265a4527a
FengFan <mail@fengfan.me>
2020-02-17 13:24:09 +0800

    feat: add case 189

---
+++ 189.rotate-array.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++) {
+        nums.unshift(nums.pop())
+    }
+}
+// @lc code=end
