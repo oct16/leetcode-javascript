351df098d93e32d1e2c21346b2cdd69389d400f5
FengFan <mail@fengfan.me>
2020-02-20 14:20:25 +0800

    feat: add case 56

--- 56.merge-intervals.js
+++ 56.merge-intervals.js
@@ -12,22 +12,15 @@
 var merge = function(intervals) {
     intervals = intervals.sort((a, b) => a[1] - b[1])
     for (let i = intervals.length - 1; i > 0; i--) {
-        const pre = intervals[i - 1]
-        const cur = intervals[i]
-        const a = pre[0]
-        const b = pre[1]
-        const c = cur[0]
-        const d = cur[1]
-
+        const [a, b] = intervals[i - 1]
+        const [c, d] = intervals[i]
         if (b < c || a > d) {
-            // no intersection condition
             continue
         } else {
             intervals[i - 1] = [Math.min(a, c), Math.max(b, d)]
             intervals.splice(i, 1)
         }
     }
-
     return intervals
 }
 // @lc code=end
