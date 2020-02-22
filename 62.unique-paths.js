8b1ceaebe629827db66a6cd30ce24b63e9941898
FengFan <mail@fengfan.me>
2020-02-22 12:44:35 +0800

    feat: add case 62

--- 62.unique-paths.js
+++ 62.unique-paths.js
@@ -10,35 +10,13 @@
  * @param {number} n
  * @return {number}
  */
-
-// var uniquePaths = function(m, n) {
-//     let res = 0
-//     function cacPath(m, n) {
-//         if (m === 1 && n === 1) {
-//             return res++
-//         }
-//         if (m > 1) {
-//             cacPath(m - 1, n)
-//         }
-//         if (n > 1) {
-//             cacPath(m, n - 1)
-//         }
-//     }
-//     cacPath(m, n)
-//     return res
-// }
-var uniquePaths = function(m, n) {
-    const memo = {}
-    function cacPath(m, n) {
-        if (m === 1 || n === 1) {
-            return 1
-        }
-        if (memo[m + ',' + n]) {
-            return memo[m + ',' + n]
-        }
-        const path = cacPath(m - 1, n) + cacPath(m, n - 1)
-        return (memo[m + ',' + n] = path)
+var uniquePaths = function(m, n, memo = {}) {
+    if (memo[m + ',' + n]) {
+        return memo[m + ',' + n]
     }
-    return cacPath(m, n)
+    if (m === 1 || n === 1) {
+        return 1
+    }
+    return (memo[m + ',' + n] = uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo))
 }
 // @lc code=end
